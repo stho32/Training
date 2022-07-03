80a604d806222ce3adb3fdead599a17f688bc8a1
stho32 <stho32github@gmail.com>
2022-07-03 18:32:35 +0200

    changed the way random topics are grabbed

---
+++ js/randomizer.js
@@ -0,0 +1,7 @@
+function displayRandomChoice(fromThisArray, intoThisDomElementId) {
+    let randomChoice = fromThisArray[Math.floor(Math.random() * fromThisArray.length)];
+    document.getElementById(intoThisDomElementId).innerText = randomChoice;
+    console.log(randomChoice);
+}
+
+
